1452f2e95e87896633862eb77ef3c9359cf1d9f0
jimsward <jim@jimsward.com>
2016-02-21 12:25:09 -0800

    Tidied up view5.js

--- public/app/view5/view5.js
+++ public/app/view5/view5.js
@@ -11,7 +11,7 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('View5Ctrl', ['$scope', '$http', '$timeout', '$location', 'getTransactions', function($scope, $http, $timeout, $location, getTransactions){
 	
 			
-	$scope.transaction = {}
+$scope.transaction = {}
 	var today = new Date()
 	, dispTo, dispFrom
 	dispTo =  today.getMonth() + 1
@@ -21,39 +21,29 @@ app.controller('View5Ctrl', ['$scope', '$http', '$timeout', '$location', 'getTra
 	$scope.transaction.from = dispFrom
 	$scope.transaction.to = dispTo
 	
-$scope.run = function(){
-	
+$scope.run = function(){	
 	var fromparts = $scope.transaction.from.split('/')
-	var from =  fromparts[2] + '/' + fromparts[0] + '/' + fromparts[1] 
-	
+	var from =  fromparts[2] + '/' + fromparts[0] + '/' + fromparts[1]	
 	var toparts = $scope.transaction.to.split('/')
-	var to =  toparts[2] + '/' + toparts[0] + '/' + toparts[1]
-		console.log(from + '-' + to )
-	var params = {"from" : from, "to" : to }
-	
-	console.dir(params)
-	
-$http({method : 'GET', url : '/transactions', params : params}).then(
-		function(response){
-	//$scope.transactions = response.data//setup array to manipulate data from the response
-	
-	
+	var to =  toparts[2] + '/' + toparts[0] + '/' + toparts[1]		
+	var params = {"from" : from, "to" : to }	
+	$http({method : 'GET', url : '/transactions', params : params}).then(	
+	function(response){		
 	var totIncome = 0, totExpense = 0
 	var totals = [], balance = 0
 	var catTotal = 0
 	, category = response.data[0].category
+	//response is sorted so when a category changes, we store a subtotal and start another category
 	response.data.forEach(function(value, index, array){
 		if (value.category == category)
 		{balance += response.data[index].amount
 		value.balance = balance
 		totals.push(value)
-		}
-		
+		}		
 		else
 		{	
 		totIncome = balance
 		totals.push( { category : category, balance : balance, split : "Total" } )	
-		
 		balance = value.amount
 		category = value.category
 		}
@@ -62,27 +52,20 @@ $http({method : 'GET', url : '/transactions', params : params}).then(
 		totals.push( { category : category, balance : balance, split : "Total" } )	
 		totals.push( { category : "Net Income", balance : totIncome - totExpense, split : "Total" } )	
 		$scope.transactions = totals
-			
-	$timeout(function(){
+		//$scope won't get updated until the next $digest; let's give it a couple seconds
+		$timeout(function(){
 		$("table tr:contains('Total')").addClass( "totals" )
 			}, 2000)
-				
-
-		})//$http
-								
-}//run
-	
+		})//$http								
+}//run	
 }]);//controller
-
 app.factory( 'getTransactions', [ '$http', function( $http ){
 	return function( params ){$http.get('/transactions', params)	
 	}	
 	} ] )
 //dates are stored as yyyy/mm/dd for sorting and somparison but are displayed as mm/dd/yyyy
 //this filter pivots the yyyy from the start to thend of the string
-app.filter('pivot', function(){
-	
-	
+app.filter('pivot', function(){	
 	return function(input){
 		if (input)
 	{
